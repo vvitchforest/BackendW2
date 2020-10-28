55c32a4c46922397fef26f0bb5b0c55c5aa8985c
irina <irina.konovalova455@gmail.com>
2020-10-28 14:02:28 +0200

    more edits

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,11 +1,10 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-
-var multer  = require('multer');
-var upload = multer({ dest: './uploads/' });
+const multer  = require('multer');
 const catController = require('../controllers/catController');
 
+const upload = multer({ dest: './uploads/' });
 
 router.get('/', catController.cat_list_get);
 
